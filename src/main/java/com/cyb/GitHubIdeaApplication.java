c25d1db5c9f92e672f7cde6624f94c5de8e06a02
孤独的滨哥 <1548320440@qq.com>
2022-07-12 00:27:08 +0800

    Initial Commit

---
+++ src/main/java/com/cyb/GitHubIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.cyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubIdeaApplication.class, args);
+    }
+
+}
