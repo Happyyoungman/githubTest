2a6da0d8e3f5d33e45fba9d2e0ae1962f878794b
孤独的滨哥 <1548320440@qq.com>
2022-07-12 00:41:08 +0800

    第二次提交测试

---
+++ src/main/java/com/cyb/ttttT.java
@@ -0,0 +1,12 @@
+package com.cyb;
+
+/**
+ * @Description: 类说明及其用途
+ * @Author 啊滨
+ * @Date 2022/7/12 0:32
+ * @PackageName:com.cyb
+ * @ClassName: ttttT
+ * @Version 1.0
+ */
+public class ttttT {
+}
